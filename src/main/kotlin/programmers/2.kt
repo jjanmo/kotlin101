0f6edbceb7cbd1aaa3eb1f934dbca68b3fd7ca95
jjanmo <jjanmo07@hanmail.net>
2024-07-20 16:30:32 +0900

    feat: 문제2 잘못된 풀이 기록

---
+++ src/main/kotlin/programmers/2.kt
@@ -0,0 +1,33 @@
+package programmers
+
+class Problem2 {
+    /** 잘못된 풀이 */
+    fun sol1(myString: String, overwriteString: String, s: Int): String {
+        val sub = myString.substring(s until s + overwriteString.length)
+        val newString = myString.replace(sub, overwriteString)
+        return newString
+    }
+}
+
+data class Quadruple<A, B, C, D>(
+    val arg1: A,
+    val arg2: B,
+    val arg3: C,
+    val output: D
+)
+
+fun main() {
+    val cases = mapOf(
+        "example" to Quadruple("He11oWor1d", "lloWorl", 2, "HelloWorld"),
+        "counterexample" to Quadruple("hahahahahahahahha", "yourselfer", 4, "hahayouretoslowha")
+    )
+
+    val problem = Problem2()
+    val (arg1, arg2, arg3, output) = cases["example"]!!
+    val result1 = problem.sol1(arg1, arg2, arg3)
+    println("$result1 $output ${result1 == output}")
+    val (cArg1, cArg2, cArg3, cOutput) = cases["counterexample"]!!
+    val result2 = problem.sol1(cArg1, cArg2, cArg3)
+    println("$result2 $cOutput ${result2 == cOutput}")
+}
+
