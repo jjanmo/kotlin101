ecbda70e9dce5408d0835419e7b9266ee50b1c78
jjanmo <jjanmo07@hanmail.net>
2024-07-20 16:39:40 +0900

    feat: 문제2 풀이 성공

--- src/main/kotlin/programmers/2.kt
+++ src/main/kotlin/programmers/2.kt
@@ -7,6 +7,12 @@ class Problem2 {
         val newString = myString.replace(sub, overwriteString)
         return newString
     }
+
+    fun sol2(myString: String, overwriteString: String, s: Int): String {
+        val pre = myString.substring(0, s)
+        val post = myString.substring(s + overwriteString.length)
+        return "$pre$overwriteString$post"
+    }
 }
 
 data class Quadruple<A, B, C, D>(
@@ -19,15 +25,25 @@ data class Quadruple<A, B, C, D>(
 fun main() {
     val cases = mapOf(
         "example" to Quadruple("He11oWor1d", "lloWorl", 2, "HelloWorld"),
-        "counterexample" to Quadruple("hahahahahahahahha", "yourselfer", 4, "hahayouretoslowha")
+        "counterexample" to Quadruple("hahahahahahahahha", "youretoslow", 4, "hahayouretoslowha")
     )
 
     val problem = Problem2()
+
     val (arg1, arg2, arg3, output) = cases["example"]!!
     val result1 = problem.sol1(arg1, arg2, arg3)
     println("$result1 $output ${result1 == output}")
     val (cArg1, cArg2, cArg3, cOutput) = cases["counterexample"]!!
     val result2 = problem.sol1(cArg1, cArg2, cArg3)
     println("$result2 $cOutput ${result2 == cOutput}")
+
+//    val (arg1, arg2, arg3, output) = cases["example"]!!
+//    val result1 = problem.sol2(arg1, arg2, arg3)
+//    println("$result1 $output ${result1 == output}")
+//    val (cArg1, cArg2, cArg3, cOutput) = cases["counterexample"]!!
+//    val result2 = problem.sol2(cArg1, cArg2, cArg3)
+//    println("$result2 $cOutput ${result2 == cOutput}")
+
+
 }
 
