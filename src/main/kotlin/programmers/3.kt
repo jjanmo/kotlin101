d8bb827784addb7826d392323d17b8be1a5d36e9
jjanmo <jjanmo07@hanmail.net>
2024-07-23 23:15:07 +0900

    문제3 풀이 완료2

--- src/main/kotlin/programmers/3.kt
+++ src/main/kotlin/programmers/3.kt
@@ -1,2 +1,22 @@
 package programmers
 
+class Problem3 {
+    fun sol1(str1: String, str2: String): String {
+        val length = str1.length
+        val sb = StringBuilder()
+        for (i in 0 until length) {
+            sb.append(str1[i]).append(str2[i])
+        }
+        return sb.toString()
+    }
+}
+
+
+fun main() {
+    print("[같은 길이 문자열1 문자열2] 입력 : ")
+    val (string1, string2) = readln().split(' ')
+
+    val problem = Problem3()
+    val result = problem.sol1(string1, string2)
+    println(result)
+}
