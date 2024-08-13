8215027b513a7704318a5d1584c36a2c1d63d1e5
jjanmo <jjanmo07@hanmail.net>
2024-08-14 01:07:42 +0900

    구구단 게임 리팩토링

--- src/main/kotlin/games/gugudan/Main.kt
+++ src/main/kotlin/games/gugudan/Main.kt
@@ -3,13 +3,25 @@ package games.gugudan
 
 fun main() {
     val range = 1..9
-    val preNumber = range.random()
-    val postNumber = range.random()
-    println("구구단 ${preNumber}단에 대한 문제입니다.")
-    println("$preNumber * $postNumber = ?")
 
-    val result = preNumber * postNumber
-    val input = readlnOrNull()
-    if (input?.toInt() == result) println("맞았습니다.")
-    else println("틀렸습니다.")
+    while (true) {
+        val preNumber = range.random()
+        val postNumber = range.random()
+        println("구구단 ${preNumber}단에 대한 문제입니다.")
+        println("$preNumber * $postNumber = ?")
+        val result = preNumber * postNumber
+
+        val input = readlnOrNull()
+        if (input.isNullOrBlank() || input.toIntOrNull() == null) {
+            println("입력 오류로 프로그램을 종료합니다.")
+            return
+        }
+
+        if (input.toInt() == result) {
+            println("맞았습니다.")
+            return
+        } else {
+            println("틀렸습니다.")
+        }
+    }
 }
