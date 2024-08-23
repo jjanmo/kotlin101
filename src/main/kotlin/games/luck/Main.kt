096d717834d28e5fefe0cc3d028a1b5d1a5836bb
jjanmo <jjanmo07@hanmail.net>
2024-08-23 23:43:52 +0900

    feat: 오늘 날짜 노출하는 로직 구현

---
+++ src/main/kotlin/games/luck/Main.kt
@@ -0,0 +1,33 @@
+package games.luck
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+import kotlin.random.Random
+
+class MyDate {
+    private val date = LocalDate.now()
+
+    private fun dayOfWeekString(month: DayOfWeek): String {
+        return when (month) {
+            DayOfWeek.MONDAY -> "월"
+            DayOfWeek.TUESDAY -> "화"
+            DayOfWeek.WEDNESDAY -> "수"
+            DayOfWeek.THURSDAY -> "목"
+            DayOfWeek.FRIDAY -> "금"
+            DayOfWeek.SATURDAY -> "토"
+            DayOfWeek.SUNDAY -> "일"
+        }
+    }
+
+    fun getToday(): String {
+        return "${date.year}년 ${date.monthValue}월 ${date.dayOfMonth}일 ${dayOfWeekString(date.dayOfWeek)}요일"
+    }
+
+}
+
+
+fun main() {
+    val date = MyDate()
+    val random = Random.nextInt(0, 100)
+    println(date.getToday())
+}
